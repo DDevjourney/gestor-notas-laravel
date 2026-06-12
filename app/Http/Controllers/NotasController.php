03f58d4838158ca4186977c97649c3db1753fd26
Carlos <carlosdsp2308coding@gmail.com>
2026-06-12 11:03:00 +0200

    Route Model Binding

--- app/Http/Controllers/NotasController.php
+++ app/Http/Controllers/NotasController.php
@@ -10,48 +10,46 @@ class NotasController extends Controller
     public function index()
     {
         $notas = Nota::orderBy('created_at')->paginate(10);
-        return view('notas.index', ['notas' => $notas]); /* Es un array asociativo con la clave 'notas' y el valor de la variable $notas,
+        return view('notas', ['notas' => $notas]); /* Es un array asociativo con la clave 'notas' y el valor de la variable $notas,
 que contiene todas las notas recogidas por Notas:all(); De esta forma, se envían todas las notas a la vista 'notas' a través de $notas. 
 La clave del array ('notas') es el nombre con el que la vista podrá acceder a los datos. El valor ($notas) son los datos en sí.*/
     }
 
     public function create()
     {
-        return view('notas.create');
+        return view('crear_nota');
     }
     public function store(Request $request)
     {
         $request->validate([
             'titulo' => 'required|string',
-            'contenido' => 'required\string'
+            'contenido' => 'required|string'
         ]);
         Nota::create([
             'titulo' => $request->titulo,
             'contenido' => $request->contenido,
         ]);
-        return redirect('notas.index');
+        return redirect('notas');
     }
-    public function destroy($id)
+    public function destroy(Nota $nota)
     {
-        Nota::destroy($id);
-        return redirect('notas.destro');
+        $nota->delete();
+        return redirect('notas');
     }
-    public function edit($id)
+    public function edit(Nota $nota)
     {
-        $nota = Nota::find($id);
-        return view('notas.edit', compact('nota'));
+        return view('editar_nota', compact('nota'));
     }
-    public function update(Request $request, $id)
+    public function update(Request $request, Nota $nota)
     {
         $request->validate([
             'titulo' => 'required',
             'contenido' => 'required'
         ]);
-        $nota = Nota::find($id);
         $nota->titulo = $request->titulo;
         $nota->contenido = $request->contenido;
         $nota->save();
-        return redirect('notas.update');
+        return redirect('notas');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,6 @@ Route::get('/', function () {
 Route::get('/notas', [NotasController::class, 'index'])->name('notas.index');
 Route::get('/crear_nota', [NotasController::class, 'create'])->name('notas.create');
 Route::post('/notas', [NotasController::class, 'store'])->name('notas.store');
-Route::delete('/notas/{id}', [NotasController::class, 'destroy'])->name('notas.destro');
-Route::get('/notas/{id}/edit', [NotasController::class, 'edit'])->name('notas.edit');
-Route::put('/notas/{id}', [NotasController::class, 'update'])->name('notas.update');
+Route::delete('/notas/{nota}', [NotasController::class, 'destroy'])->name('notas.destro');
+Route::get('/notas/{nota}/edit', [NotasController::class, 'edit'])->name('notas.edit');
+Route::put('/notas/{nota}', [NotasController::class, 'update'])->name('notas.update');
